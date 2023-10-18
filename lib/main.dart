ae90aa5adb3aa9722dfda5913b8be26426adf27b
oscar.mendez <oscar.mendez@indimin.com>
2023-10-17 23:01:42 -0600

    terminando vists de clientes y  facturas

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,8 @@ class MyApp extends StatelessWidget {
   }
 }
 
+//--- DESDE AQUI MIGRAR
+
 class Home extends StatelessWidget {
   const Home({super.key});
 
@@ -30,41 +32,27 @@ class Home extends StatelessWidget {
       length: 2,
       child: Scaffold(
         appBar: AppBar(
-          bottom: const TabBar(
-            tabs: [
-              Tab(
-                icon: Icon(Icons.list),
-                text: 'Facturas',
-              ),
-              Tab(
-                icon: Icon(Icons.edit),
-                text: 'Productos / Clientes',
-              ),
-            ],
-          ),
           title: const Text('Merendero Suyapa'),
         ),
-        body: const TabBarView(
-          children: [
-            Invoices(),
-            ProductsClients(),
-          ],
-        ),
+        body: const Clients(),
       ),
     ));
   }
 }
 
-class Invoices extends StatelessWidget {
-  const Invoices({super.key});
+class Clients extends StatelessWidget {
+  const Clients({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: Padding(
-          padding: const EdgeInsets.all(12.0),
+          padding: const EdgeInsets.all(10),
           child: SingleChildScrollView(
             child: Column(children: [
+              const SizedBox(
+                height: 20,
+              ),
               const TextField(
                 decoration: InputDecoration(
                   prefixIcon: Icon(Icons.search),
@@ -77,37 +65,135 @@ class Invoices extends StatelessWidget {
                   padding: const EdgeInsets.all(8),
                   itemCount: 5,
                   itemBuilder: (BuildContext context, int index) {
-                    return const Card(
-                      child: ListTile(
-                        leading: Text('Ivan Calderon'),
-                        title: Text('2 - Baleadas - 10/Oct/2023'),
-                        subtitle: Text('L 50'),
+                    return Card(
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.white,
+                            foregroundColor: Colors.blue),
+                        onPressed: () {
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (context) => const Invoices(
+                                    client: 'Ivan Calderon',
+                                  )));
+                        },
+                        child: const ListTile(
+                          leading: CircleAvatar(
+                            child: Text(
+                              'IC',
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          title: Text('Ivan Calderon'),
+                        ),
                       ),
                     );
                   })
             ]),
           ),
         ),
-        persistentFooterButtons: const [Text('Total ' 'L 300')],
         floatingActionButton: FloatingActionButton(
-          onPressed: () {},
+          onPressed: () {
+            showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    title: const Text("Agregar cliente"),
+                    content: SizedBox(
+                        height: 110,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            const TextField(
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: 'Nombre del cliente',
+                              ),
+                            ),
+                            ElevatedButton(
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                                child: const Text('Guardar'))
+                          ],
+                        )),
+                  );
+                });
+          },
           tooltip: 'Increment',
           child: const Icon(Icons.add),
         ));
   }
 }
 
-class ProductsClients extends StatelessWidget {
-  const ProductsClients({super.key});
+class Invoices extends StatelessWidget {
+  final String client;
+  const Invoices({super.key, required this.client});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(children: []),
+        appBar: AppBar(title: Text('Facturas de $client')),
+        body: ListView.builder(
+            shrinkWrap: true,
+            padding: const EdgeInsets.all(8),
+            itemCount: 5,
+            itemBuilder: (BuildContext context, int index) {
+              return Card(
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      foregroundColor: Colors.blue),
+                  onPressed: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => const ViewInvoice()));
+                  },
+                  child: const ListTile(
+                      leading: CircleAvatar(
+                        child: Icon(Icons.format_list_bulleted),
+                      ),
+                      title: Text('17-oct-2023'),
+                      trailing: Text('L. 100')),
+                ),
+              );
+            }),
+        persistentFooterButtons: const [Text('Total L. 500')],
         floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          tooltip: 'Increment',
+          onPressed: () {
+            Navigator.of(context).push(MaterialPageRoute(
+                builder: (context) => const AddInvoice(
+                      client: 'Ivan Calderon',
+                    )));
+          },
           child: const Icon(Icons.add),
         ));
+  }
+}
+
+class AddInvoice extends StatefulWidget {
+  final String client;
+  const AddInvoice({super.key, required this.client});
+
+  @override
+  State<AddInvoice> createState() => _AddInvoiceState();
+}
+
+class _AddInvoiceState extends State<AddInvoice> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Nueva factura de ${widget.client}')),
+    );
+  }
+}
+
+class ViewInvoice extends StatelessWidget {
+  const ViewInvoice({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Detalle de factura')),
+    );
   }
 }
